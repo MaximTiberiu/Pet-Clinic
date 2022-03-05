111c8bb038dbea5a0ba4fc9ad68efcde2bae8246
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-03-05 19:33:26 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/tiberiumaxim/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.tiberiumaxim.petclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/tiberiumaxim/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.tiberiumaxim.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
