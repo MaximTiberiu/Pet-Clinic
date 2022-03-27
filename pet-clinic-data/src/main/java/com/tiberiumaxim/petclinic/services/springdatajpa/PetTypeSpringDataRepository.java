de8f45e27e088a9509118775e3dd566e1019e70f
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-03-27 23:50:47 +0300

    Added Spring Data JPA Service for Pet Types. Closes #44.

---
+++ pet-clinic-data/src/main/java/com/tiberiumaxim/petclinic/services/springdatajpa/PetTypeSpringDataRepository.java
@@ -0,0 +1,50 @@
+package com.tiberiumaxim.petclinic.services.springdatajpa;
+
+import com.tiberiumaxim.petclinic.models.PetType;
+import com.tiberiumaxim.petclinic.repositories.PetTypeRepository;
+import com.tiberiumaxim.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class PetTypeSpringDataRepository implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSpringDataRepository(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+
+        Optional<PetType> petTypeOptional = petTypeRepository.findById(id);
+        return petTypeOptional.orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
