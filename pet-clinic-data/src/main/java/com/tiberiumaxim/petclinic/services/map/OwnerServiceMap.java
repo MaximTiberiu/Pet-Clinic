c8865c90de6d387b787ce593be641d0f6d06dc77
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-03-27 22:16:28 +0300

    Added Pets and Contact Info to Owners on Startup. Closes #34.

--- pet-clinic-data/src/main/java/com/tiberiumaxim/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/tiberiumaxim/petclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,25 @@
 package com.tiberiumaxim.petclinic.services.map;
 
 import com.tiberiumaxim.petclinic.model.Owner;
+import com.tiberiumaxim.petclinic.model.Pet;
 import com.tiberiumaxim.petclinic.services.OwnerService;
+import com.tiberiumaxim.petclinic.services.PetService;
+import com.tiberiumaxim.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,7 +37,25 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+         if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        pet.setPetType(petTypeService.save(pet.getPetType()));
+                    } else {
+                        throw new RuntimeException("Pet Type is Required!");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }
+        return null;
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/tiberiumaxim/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tiberiumaxim/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.tiberiumaxim.petclinic.bootstrap;
 
 import com.tiberiumaxim.petclinic.model.Owner;
+import com.tiberiumaxim.petclinic.model.Pet;
 import com.tiberiumaxim.petclinic.model.PetType;
 import com.tiberiumaxim.petclinic.model.Vet;
 import com.tiberiumaxim.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.tiberiumaxim.petclinic.services.PetTypeService;
 import com.tiberiumaxim.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,18 +38,38 @@ public class DataLoader implements CommandLineRunner {
 		cat.setName("Cat");
 		PetType savedCatPetType = petTypeService.save(cat);
 
-
+		System.out.println("Loaded Pet Types...");
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("123 Bricks Street");
+		owner1.setCity("Miami");
+		owner1.setTelephone("123456789");
 
+		Pet mikesPet = new Pet();
+		mikesPet.setPetType(savedDogPetType);
+		mikesPet.setOwner(owner1);
+		mikesPet.setBirthDate(LocalDate.now());
+		mikesPet.setName("Rosco");
+
+		owner1.getPets().add(mikesPet);
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Smith");
+		owner2.setAddress("13 Victory Street");
+		owner2.setCity("New York");
+		owner2.setTelephone("987654321");
 
+		Pet fionasCat = new Pet();
+		fionasCat.setPetType(savedCatPetType);
+		fionasCat.setOwner(owner2);
+		fionasCat.setBirthDate(LocalDate.now());
+		fionasCat.setName("Lisi");
+
+		owner2.getPets().add(fionasCat);
 		ownerService.save(owner2);
 
 		System.out.println("Loaded Owners...");
