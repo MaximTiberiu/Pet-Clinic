9582b80aa9a82989ee47d1b38a7bde9a7a335962
Tiberiu Maxim <maxim.tiberiu.info@gmail.com>
2022-03-28 13:01:19 +0300

    Added Project Lombok and Refactored. Closes #52.

--- pet-clinic-web/src/main/java/com/tiberiumaxim/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tiberiumaxim/petclinic/bootstrap/DataLoader.java
@@ -21,7 +21,7 @@ public class DataLoader implements CommandLineRunner {
 	private final VisitService visitService;
 
 	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-					  SpecialtyService specialtyService, VisitService visitService) {
+			SpecialtyService specialtyService, VisitService visitService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
